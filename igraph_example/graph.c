b33e45c33557e1389b2d071143c93afc8064a689
sorianpd <sorianpd@dukes.jmu.edu>
2017-03-21 20:01:23 -0400

    added a test file, graph.c makes a graph -- need to figure out vertex argument

--- igraph_example/graph.c
+++ igraph_example/graph.c
@@ -27,27 +27,41 @@ void print_vector(igraph_vector_t *v, FILE *f) {
   fprintf(f, "\n");
 }
 
-int main() {
+int main(int argc, char* argv[]) {
 
   igraph_t g;
+  igraph_vit_t vit;
   igraph_vector_t v;
   int ret;
 
   // Create graph
-  igraph_vector_init(&v, 0);
+  igraph_vector_init(&v, 156);
   FILE *file = fopen (argv[1], "r");
-  int f, t;
-  while(fscanf(file, "%d %d", &f, &t) > 0) {
-     VECTOR(v)[f]=t;
+  int f;
+  int index; 
+  while(fscanf(file, "%d", &f) > 0) {
+     VECTOR(v)[index++] = f;
   }
   fclose(file);
   igraph_create(&g, &v, 0, IGRAPH_UNDIRECTED);
+  
+  // Create an vit (iterator) through all of the vertices
+  igraph_vit_create(&g, igraph_vss_all(), &vit);
 
+  // Get the amount of items in the iterator
+  printf("Size: %ld\n\n", IGRAPH_VIT_SIZE(vit));
+  
   // Check result
   igraph_get_edgelist(&g, &v, 0);
   igraph_vector_sort(&v);
   print_vector(&v, stdout);
-
+  printf ("edge_count: %d\n", igraph_ecount(&g));
+  igraph_write_graph_edgelist(&g, stdout);
+  
+  // Destroy resources
+  igraph_vit_destroy(&vit);
+  igraph_vector_destroy(&v);
+  igraph_destroy(&g);
 
   return 0;
 }

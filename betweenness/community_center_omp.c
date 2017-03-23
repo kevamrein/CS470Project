53ce970b42a55db7da648deb7ca03662b2f752f5
20170109 <amreinkh@dukes.jmu.edu>
2017-03-23 11:53:14 -0400

    Working max_betweenness reduction but need to figure out how to get the vertex from that

--- betweenness/community_center_omp.c
+++ betweenness/community_center_omp.c
@@ -18,29 +18,30 @@ int main(int argc, char *argv[]) {
     int vcount = igraph_vcount(&graph);
     int max_vertex = -1;
     double max_betweenness = -1;
-    igraph_vector_t result;
-    igraph_vector_init(&result, 0);
+    double betweenness;
     printf ("vertices: %d, edges: %d\n", vcount, igraph_ecount(&graph)); 
 
-//#   pragma omp parallel for default(none) shared(graph, max_vertex, max_betweenness) private(i)
+#   pragma omp parallel for default(none) shared(vcount, graph, max_vertex) \
+    private(betweenness, i) reduction(max: max_betweenness)
    for (i = 0; i < vcount; i++) {
         igraph_vs_t vs;
+        igraph_vector_t result;
+        igraph_vector_init(&result, 0);
         //igraph_vs_seq(&vs, i, i);
         igraph_vs_1(&vs, i);
         igraph_betweenness(&graph, &result, vs, IGRAPH_UNDIRECTED, 0, 1);
-        double betweenness = (double) VECTOR(result)[0];
-        printf ("betweenness: %f\n", betweenness);
-//#       pragma omp critical
-       // {
+        betweenness = (double) VECTOR(result)[0];
+        printf ("i: %d betweenness: %f\n", i, betweenness);
         if (betweenness > max_betweenness) {
             max_betweenness = betweenness;
-            max_vertex = i; 
+            max_vertex = i;
+            //printf("i: %d\n", i);
         }
-      //  } 
-    } 
+        igraph_vs_destroy(&vs);
+        igraph_vector_destroy(&result);
+    }
     printf ("max vertex: %d, max_b: %f\n", max_vertex, max_betweenness);    
     
-    igraph_vector_destroy(&result);
     igraph_destroy(&graph);
     return 0;
 }

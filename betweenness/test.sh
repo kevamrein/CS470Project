15180858799f26646d8600312f608664500b1202
20170109 <sorianpd@dukes.jmu.edu>
2017-03-26 18:36:32 -0400

    made script pretty

--- betweenness/test.sh
+++ betweenness/test.sh
@@ -8,10 +8,10 @@ echo "Using Zachary's Karate Data Set"
 make clean && make cluster
 
 echo "Method #2 Betweenness Centrality BC"
-echo "Serial Version"
+echo "--Serial Version--"
 ./bc data/edges.txt
 
-echo "OMP Version"
+echo "--OMP Version--"
 OMP_NUM_THREADS=1 ./bc data/edges.txt
 OMP_NUM_THREADS=2 ./bc data/edges.txt
 OMP_NUM_THREADS=4 ./bc data/edges.txt
@@ -19,10 +19,10 @@ OMP_NUM_THREADS=8 ./bc data/edges.txt
 
 echo ""
 echo "Method #3 Degree and Line Importance"
-echo "Serial Version"
+echo "--Serial Version--"
 ./dil data/edges.txt
 
-echo "OMP Version"
+echo "--OMP Version--"
 OMP_NUM_THREADS=1 ./dil data/edges.txt
 OMP_NUM_THREADS=2 ./dil data/edges.txt
 OMP_NUM_THREADS=4 ./dil data/edges.txt

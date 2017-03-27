23da96bb1b3c2d6e5da05dfc677f21b7ba95b84e
20170109 <amreinkh@dukes.jmu.edu>
2017-03-26 23:04:55 -0400

    Fixed DIL

--- betweenness/test.sh
+++ betweenness/test.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 #
 #SBATCH --job-name=mid-proj
-#SBATCH --output=out
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
 
 echo "Using Zachary's Karate Data Set"
 
-make clean 
-make
+`make`
 
 echo "Method #2 Betweenness Centrality BC"
 echo "--Serial Version--"
 ./bc data/edges.txt
 
 echo "--OMP Version--"
-OMP_NUM_THREADS=1 ./bc data/edges.txt
 OMP_NUM_THREADS=2 ./bc data/edges.txt
 OMP_NUM_THREADS=4 ./bc data/edges.txt
 OMP_NUM_THREADS=8 ./bc data/edges.txt
@@ -21,10 +20,9 @@ OMP_NUM_THREADS=8 ./bc data/edges.txt
 echo ""
 echo "Method #3 Degree and Line Importance"
 echo "--Serial Version--"
-./dil data/edges.txt
+OMP_NUM_THREADS=1 ./dil data/edges.txt
 
 echo "--OMP Version--"
-OMP_NUM_THREADS=1 ./dil data/edges.txt
 OMP_NUM_THREADS=2 ./dil data/edges.txt
 OMP_NUM_THREADS=4 ./dil data/edges.txt
 OMP_NUM_THREADS=8 ./dil data/edges.txt

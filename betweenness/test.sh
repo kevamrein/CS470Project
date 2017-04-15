bb62477b7d212060bafe550885131b3c2d7a9035
20170109 <sorianpd@dukes.jmu.edu>
2017-04-15 14:44:55 -0400

    testing

--- betweenness/test.sh
+++ betweenness/test.sh
@@ -7,24 +7,24 @@
 echo "Using Zachary's Karate Data Set"
 
 make
-TXT="data/edges.txt" 
+TXT="data/lesmis.txt" 
 
 echo "Method #2 Betweenness Centrality BC"
 echo "--Serial Version--"
-OMP_NUM_THREADS=1 ./bc $TXT
+#OMP_NUM_THREADS=1 ./bc $TXT
 
 echo "--OMP Version--"
 OMP_NUM_THREADS=2 ./bc $TXT
-OMP_NUM_THREADS=4 ./bc $TXT
-OMP_NUM_THREADS=8 ./bc $TXT
+#OMP_NUM_THREADS=4 ./bc $TXT
+#OMP_NUM_THREADS=8 ./bc $TXT
 
 echo ""
 echo "Method #3 Degree and Line Importance"
 echo "--Serial Version--"
-./dil-serial $TXT
+#./dil-serial $TXT
 
 echo "--OMP Version--"
 OMP_NUM_THREADS=2 ./dil $TXT
-OMP_NUM_THREADS=4 ./dil $TXT
-OMP_NUM_THREADS=8 ./dil $TXT
+#OMP_NUM_THREADS=4 ./dil $TXT
+#OMP_NUM_THREADS=8 ./dil $TXT
 
